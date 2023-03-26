526bd22963db78af5ed36098d9f7740aa16f9d01
Arthur Bicalho <arthurbic95@gmail.com>
2023-03-26 19:23:06 -0300

    file mongodb

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import connectDB from './mongoDB/connect.js';
+import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import iaRoutes from './routes/iaRoutes.js';
 
