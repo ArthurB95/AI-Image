52855c3e9a19422c95b4ce13250763599b92f6ba
Arthur Bicalho <arthurbic95@gmail.com>
2023-03-27 21:33:02 -0300

    README

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,7 +15,7 @@ export const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/ia", {
+        const response = await fetch("https://aiimage-l5i8.onrender.com/api/v1/ia", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -42,7 +42,7 @@ export const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://aiimage-l5i8.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
